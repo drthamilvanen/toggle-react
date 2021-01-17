8bf44ece40b187fdcf797a0fcdb295ab0a7a1ad2
Thamil <thamilpro@gmail.com>
2021-01-17 17:08:30 +0530

    Toggle Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./assets/css/style.css";
 
-function App() {
+/*function App({title}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-gray-600 text-white p-5 border">{title}</div>
     </div>
   );
+}*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "Hello Mr", isShowing: false };
+  }
+  render() {
+    return (
+      <section className="flex justify-center">
+        <div className="w-1/2">
+          <div className="text-center">
+            <div className="my-4">{this.state.title}</div>
+            <button
+              className="p-1 bg-blue-700 text-white my-2"
+              onClick={() =>
+                this.setState({ isShowing: !this.state.isShowing })
+              }>
+              Toggle Image
+            </button>
+          </div>
+        {
+            this.state.isShowing ? (
+            <img 
+            src="https://images.unsplash.com/photo-1610748604799-dbec8cc22cd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+            ): null
+        }
+          
+     
+          
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
