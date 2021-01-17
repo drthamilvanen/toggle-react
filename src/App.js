1981c1704f80562653a1f8c0661198f2a80a3d67
Thamil <thamilpro@gmail.com>
2021-01-17 19:32:03 +0530

    Changed to react functional interface

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import { render } from "@testing-library/react";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./assets/css/style.css";
+import Images from "./components/images";
 
-/*function App({title}) {
+
+
+
+
+function App() {
+  const [title, setTitle] = React.useState("Hello React");
+  const [isShowing, setisShowing] = React.useState(false);
+
+  function handleClick(){
+    setisShowing(!isShowing);
+
+  }
+
   return (
-    <div>
-      <div className="bg-gray-600 text-white p-5 border">{title}</div>
+    <section className="flex justify-center">
+    <div className="w-1/2">
+      <div className="text-center">
+        <div className="my-4">{title}</div>
+        <button
+          className="p-1 bg-blue-700 text-white my-2"
+          onClick={handleClick}>
+          Toggle Image
+        </button>
+      </div>
+    {
+        isShowing ? <Images/> : null
+    }
+      
+ 
+      
     </div>
+  </section>
   );
-}*/
-class App extends Component {
+}
+/*class App extends Component {
   constructor(props) {
     super(props);
     this.state = { title: "Hello Mr", isShowing: false };
   }
+
+  handleClick =() =>{
+    this.setState({ isShowing: !this.state.isShowing })
+  }
+
   render() {
     return (
       <section className="flex justify-center">
@@ -22,17 +55,12 @@ class App extends Component {
             <div className="my-4">{this.state.title}</div>
             <button
               className="p-1 bg-blue-700 text-white my-2"
-              onClick={() =>
-                this.setState({ isShowing: !this.state.isShowing })
-              }>
+              onClick={this.handleClick}>
               Toggle Image
             </button>
           </div>
         {
-            this.state.isShowing ? (
-            <img 
-            src="https://images.unsplash.com/photo-1610748604799-dbec8cc22cd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
-            ): null
+            this.state.isShowing ? <Images/> : null
         }
           
      
@@ -41,6 +69,6 @@ class App extends Component {
       </section>
     );
   }
-}
+}*/
 
 export default App;
---
+++ src/components/images.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+class Images extends Component {
+    state = {  }
+    render() { 
+        return (<img 
+            src="https://images.unsplash.com/photo-1610748604799-dbec8cc22cd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+            );
+    }
+}
+ 
+export default Images;
